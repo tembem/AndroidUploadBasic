1642a9a9430ce86e29c16c13fe9a080aec5a3498
Jimmy Chandra <jim@tembem.com>
2016-04-26 18:11:31 +0700

    Prepare initial layout for user interface

--- app/src/main/java/com/tembem/app/androiduploadbasic/MainActivity.java
+++ app/src/main/java/com/tembem/app/androiduploadbasic/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
     }
 
     @Override
